907e9d974862745875d64933b659c92689b678a8
SlayerRunner008 <mjv80547@lasallebajio.edu.mx>
2025-06-16 19:32:37 -0600

    Adición de metodos get por id y put

--- Sources/GamesApi/routes.swift
+++ Sources/GamesApi/routes.swift
@@ -17,10 +17,35 @@ app.get { req async in
         return games
     }
 
+    app.get("games",":id"){
+        req async throws -> Game in 
+        guard let game = try await Game.find(req.parameters.get("id"),on:req.db) else {
+           throw Abort(.notFound,reason:"El registro no fue encontrado")
+        }
+        return game
+    }
+
     app.post("games"){ req async throws -> Game in 
          let game = try req.content.decode(Game.self)
          try await game.save(on: req.db)
          return game
+    }
+
+    app.put("games",":id"){
+        req async throws -> Game in 
+        guard let ExistingGame = try await Game.find(req.parameters.get("id"),on:req.db) else {
+           throw Abort(.notFound,reason:"El registro no fue encontrado")
+        }
+
+        let updatedGame = try req.content.decode(Game.self)
+
+        ExistingGame.title = updatedGame.title
+        ExistingGame.image = updatedGame.image
+        ExistingGame.description = updatedGame.description
+        ExistingGame.publisher = updatedGame.publisher
+
+        try await ExistingGame.update(on:req.db)
+        return ExistingGame
     }
 
     
